85d6e62fc66a139ddacb899a52eec68515cf80bd
NourelhoudaOKMID <nour.okmid@gmail.com>
2026-02-13 12:49:12 +0100

    follow us section

---
+++ src/pages/home/partials/index.jsx
@@ -0,0 +1,46 @@
+
+ export const Followussection = () => {
+  return (
+    <>
+    <section className="p-b-3  px-4"> 
+      <h2 className="text-lg font-medium text-center">
+        @ FOLLOW US ON INSTAGRAM
+      </h2> </section>
+    <section className="bg-white py-16" >
+
+   
+      <div className="flex flex-wrap px-4">
+        
+        <div className="flex flex-col items-center w-full sm:w-1/3 px-4 py-4">
+          <h4 className="text-lg font-semibold text-center">
+            Free Delivery Worldwide
+          </h4>
+          <span className="text-sm text-center text-gray-600">
+            Mirum est notare quam littera gothica
+          </span>
+        </div>
+
+        <div className="flex flex-col items-center w-full sm:w-1/3 px-4 py-4 border-l border-r border-gray-200">
+          <h4 className="text-lg font-semibold text-center">
+            30 Days Return
+          </h4>
+          <span className="text-sm text-center text-gray-600">
+            Simply return it within 30 days for an exchange.
+          </span>
+        </div>
+
+        <div className="flex flex-col items-center w-full sm:w-1/3 px-4 py-4">
+          <h4 className="text-lg font-semibold text-center">
+            Store Opening
+          </h4>
+          <span className="text-sm text-center text-gray-600">
+            Shop open from Monday to Sunday
+          </span>
+        </div>
+
+      </div>
+    </section>
+    </>
+  );
+};
+
