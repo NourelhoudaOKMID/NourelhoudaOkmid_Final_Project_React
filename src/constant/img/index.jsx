bbbb98a6ee8fa28295bffbf871d2cc7e0b86303b
NourelhoudaOKMID <nour.okmid@gmail.com>
2026-02-13 12:54:45 +0100

    import images

---
+++ src/constant/img/index.jsx
@@ -0,0 +1,20 @@
+import gris from "../../../src/assets/images/gris.png";
+import marron from "../../../src/assets/images/marron.png";
+import men from "../../../src/assets/images/men.jpg";
+import men2 from "../../../src/assets/images/MEN2.jpg";
+import men4 from "../../../src/assets/images/MEN4.jpg";
+import p1 from "../../../src/assets/images/pl.jpg";
+import hoodie from  "../../../src/assets/images/hoodies.jpg";
+
+
+
+
+export const Images =  {
+    gris,
+    marron,
+    men,
+    men2,
+    men4,
+    p1,
+    hoodie,
+};
