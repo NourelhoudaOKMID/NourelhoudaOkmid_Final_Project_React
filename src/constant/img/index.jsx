edd8f52b3f17fb08dabb9e9840550123ade3eab5
NourelhoudaOKMID <nour.okmid@gmail.com>
2026-02-16 02:15:32 +0000

    import and add the assets

--- src/constant/img/index.jsx
+++ src/constant/img/index.jsx
@@ -1,20 +1,37 @@
-import gris from "../../../src/assets/images/gris.png";
 import marron from "../../../src/assets/images/marron.png";
 import men from "../../../src/assets/images/men.jpg";
 import men2 from "../../../src/assets/images/MEN2.jpg";
 import men4 from "../../../src/assets/images/MEN4.jpg";
 import p1 from "../../../src/assets/images/pl.jpg";
 import hoodie from  "../../../src/assets/images/hoodies.jpg";
-
-
-
+import blog1 from  "../../../src/assets/images/blogg1.jpg";
+import blog2 from  "../../../src/assets/images/blogg3.jpg";
+import blog3 from  "../../../src/assets/images/blogg2.jpg";
+import tshirt from "../../../src/assets/images/T-shirt.jpg";
+import collection from "../../../src/assets/images/collection.jpg";
+import sportabaa from "../../../src/assets/images/sportabaa1.jpg";
+import sunglasses from "../../../src/assets/images/sunglasses1.jpg";
+import watch from "../../../src/assets/images/watch1.jpg";
+import footwear from "../../../src/assets/images/footwear.jpg";
+import bag from "../../../src/assets/images/bag.jpg";
+import accessories from "../../../src/assets/images/accessories1.jpg";
 
 export const Images =  {
-    gris,
+    collection,
     marron,
     men,
     men2,
     men4,
-    p1,
+    p1, 
     hoodie,
+    blog1,
+    blog2, 
+    blog3, 
+    tshirt,
+    sportabaa,
+    sunglasses,
+    watch,
+    footwear,
+    bag,
+    accessories,
 };
